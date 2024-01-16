17090a27bfd49447640a11f8baafe64fffd9ecce
bee-mcc <tuj03791@temple.edu>
2024-01-15 21:29:37 -0500

    adds game cycle and win state

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,7 @@
 let img; // Declare variable 'img'.
+let answerImage;
 let startTime;
+let gameStarted = false;
 let answer = [
   [310, 380],
   [275, 350],
@@ -8,42 +10,58 @@ let answer = [
 function setup() {
   createCanvas(1080, 600);
   img = loadImage('pic.png'); // Load the image
-  startTime = new Date();
+
+  answerImage = loadImage('answer.png');
 }
 
 function draw() {
-  img.resize(1080, 600);
+  if (gameStarted) {
+    // Displays the image at its actual size at point (0,0)
+    img.resize(1080, 600);
+    image(img, 0, 0);
 
-  // Displays the image at its actual size at point (0,0)
-  image(img, 0, 0);
+    // Get the mouse coordinates relative to the image
+    let mouseXOnImage = mouseX;
+    let mouseYOnImage = mouseY;
 
-  // Get the mouse coordinates relative to the image
-  let mouseXOnImage = mouseX;
-  let mouseYOnImage = mouseY;
+    // Display the cursor position on the image
+    fill(255);
+    noStroke();
+    ellipse(mouseXOnImage, mouseYOnImage, 10, 10);
 
-  // Display the cursor position on the image
-  fill(255);
-  noStroke();
-  ellipse(mouseXOnImage, mouseYOnImage, 10, 10);
+    // Display the cursor position below the image
+    fill(255);
+    text(
+      `Cursor Position: (${mouseXOnImage}, ${mouseYOnImage})`,
+      10,
+      height - 10
+    );
+  } else {
+    image(answerImage, 0, 0);
 
-  // Display the cursor position below the image
-  fill(255);
-  text(
-    `Cursor Position: (${mouseXOnImage}, ${mouseYOnImage})`,
-    10,
-    height - 10
-  );
+    text(
+      `Find this image hidden somewhere within the image. Click to start your time`,
+      0,
+      150
+    );
+  }
 }
 
 function mouseClicked() {
-  const xCorrect = mouseX > answer[0][0] && mouseX < answer[0][1];
-  const yCorrect = mouseY > answer[1][0] && mouseY < answer[1][1];
-  const isCorrect = xCorrect && yCorrect;
-  if (isCorrect) {
-    const endTime = new Date();
-    const seconds = (endTime.valueOf() - startTime.valueOf()) / 1000;
-    alert(seconds);
+  if (gameStarted) {
+    const xCorrect = mouseX > answer[0][0] && mouseX < answer[0][1];
+    const yCorrect = mouseY > answer[1][0] && mouseY < answer[1][1];
+    const isCorrect = xCorrect && yCorrect;
+    if (isCorrect) {
+      const endTime = new Date();
+      const seconds =
+        (endTime.valueOf() - startTime.valueOf()) / 1000;
+      alert(`You found the answer in ${seconds} seconds`);
+    } else {
+      alert("That's not the right answer...");
+    }
   } else {
-    alert('awww...');
+    gameStarted = true;
+    startTime = new Date();
   }
 }
