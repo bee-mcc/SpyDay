77b34183287748703481c36403043ae82b6a61f3
bee-mcc <tuj03791@temple.edu>
2024-01-16 14:27:30 -0500

    adds loading screen

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,12 @@
 let img; // Declare variable 'img'.
 let answerImage;
+let loadingImage;
+
 let startTime;
+
+let isImageLoading = true;
+let isAnswerLoading = true;
+
 let gameStarted = false;
 let answer = [
   [310, 380],
@@ -8,64 +14,81 @@ let answer = [
 ];
 
 function preload() {
-  img = loadImage('pic.png'); // Load the image
-  answerImage = loadImage('answer.png');
+  loadingImage = createImg('loading.gif');
 }
 
 function setup() {
+  img = loadImage('pic.png', () => (isImageLoading = false)); // Load the image
+  answerImage = loadImage(
+    'answer.png',
+    () => (isAnswerLoading = false)
+  );
   createCanvas(1080, 600);
   noLoop(); //If using noLoop() in setup(), it should be the last line inside the block.
 }
 
 function draw() {
-  if (gameStarted) {
-    // Displays the image at its actual size at point (0,0)
-    img.resize(1080, 600);
-    image(img, 0, 0);
+  const isLoading = isImageLoading || isAnswerLoading;
 
-    // Get the mouse coordinates relative to the image
-    let mouseXOnImage = mouseX;
-    let mouseYOnImage = mouseY;
-
-    // Display the cursor position on the image
-    // fill(255);
-    // noStroke();
-    // ellipse(mouseXOnImage, mouseYOnImage, 10, 10);
-
-    // Display the cursor position below the image
-    fill(255);
-    text(
-      `Cursor Position: (${mouseXOnImage}, ${mouseYOnImage})`,
-      10,
-      height - 10
-    );
+  if (isLoading) {
+    loadingImage.position(0, 0);
+    loop();
   } else {
-    image(answerImage, 0, 0);
+    loadingImage.remove();
+    noLoop();
+    if (gameStarted) {
+      // Displays the image at its actual size at point (0,0)
+      img.resize(1080, 600);
+      image(img, 0, 0);
 
-    text(
-      `Find this image hidden somewhere within the image. Click to start your time`,
-      0,
-      150
-    );
+      // Get the mouse coordinates relative to the image
+      let mouseXOnImage = mouseX;
+      let mouseYOnImage = mouseY;
+
+      // Display the cursor position on the image
+      // fill(255);
+      // noStroke();
+      // ellipse(mouseXOnImage, mouseYOnImage, 10, 10);
+
+      // Display the cursor position below the image
+      fill(255);
+      text(
+        `Cursor Position: (${mouseXOnImage}, ${mouseYOnImage})`,
+        10,
+        height - 10
+      );
+    } else {
+      image(answerImage, 0, 0);
+
+      text(
+        `Find this image hidden somewhere within the image. Click to start your time`,
+        0,
+        150
+      );
+    }
   }
 }
 
 function touchEnded() {
-  if (gameStarted) {
-    const xCorrect = mouseX > answer[0][0] && mouseX < answer[0][1];
-    const yCorrect = mouseY > answer[1][0] && mouseY < answer[1][1];
-    const isCorrect = xCorrect && yCorrect;
-    if (isCorrect) {
-      const endTime = new Date();
-      const seconds =
-        (endTime.valueOf() - startTime.valueOf()) / 1000;
-      alert(`You found the answer in ${seconds} seconds`);
+  const isLoading = isImageLoading || isAnswerLoading;
+
+  if (!isLoading) {
+    if (gameStarted) {
+      const xCorrect = mouseX > answer[0][0] && mouseX < answer[0][1];
+      const yCorrect = mouseY > answer[1][0] && mouseY < answer[1][1];
+      const isCorrect = xCorrect && yCorrect;
+      if (isCorrect) {
+        const endTime = new Date();
+        const seconds =
+          (endTime.valueOf() - startTime.valueOf()) / 1000;
+        alert(`You found the answer in ${seconds} seconds`);
+      } else {
+        alert("That's not the right answer...");
+      }
     } else {
-      alert("That's not the right answer...");
+      gameStarted = true;
+      startTime = new Date();
+      redraw();
     }
-  } else {
-    gameStarted = true;
-    startTime = new Date();
-    redraw();
   }
 }
