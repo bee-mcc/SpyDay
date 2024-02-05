b9c97305df96067c9f8c63eef6f0bf21a8987c2d
bee-mcc <tuj03791@temple.edu>
2024-02-05 07:49:09 -0500

    reduce time hint text is on screen

--- public/sketch.js
+++ public/sketch.js
@@ -185,7 +185,7 @@ function displayImage() {
 
   if (xScrollingIsEnabled) {
     frameWhenUserStarted = frameWhenUserStarted ?? frameCount;
-    if (frameCount < frameWhenUserStarted + 450) {
+    if (frameCount < frameWhenUserStarted + 180) {
       textAlign(CENTER, CENTER);
       textSize(32);
       fill(255);
