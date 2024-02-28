348f918454bb8736e8c9496936220277db263d4d
bee-mcc <tuj03791@temple.edu>
2024-02-27 20:47:19 -0500

    tests

--- public/sketch.js
+++ public/sketch.js
@@ -4,16 +4,5 @@ function setup() {
 
 function draw() {
   const myInput = createInput();
-  const onInput = function () {
-    let userInput = this.value();
-
-    if (userInput.length > 3) {
-      this.value(userInput.slice(0, 3));
-    } else {
-      this.value(userInput);
-    }
-  };
-
-  myInput.input(onInput);
   myInput.position(window.innerWidth / 2 - 75, 520);
 }
