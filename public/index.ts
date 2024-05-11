1e2951bec42582999a00712d2236390aadd2970f
bee-mcc <tuj03791@temple.edu>
2024-05-11 14:00:46 -0400

    covers with black

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,7 @@
 window.addEventListener('load', () => {
   const image = document.getElementById('smokey') as HTMLImageElement;
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
 
   function adjustImagePosition() {
     const windowWidth = window.innerWidth;
@@ -13,9 +15,22 @@ window.addEventListener('load', () => {
     image.style.position = 'fixed';
     image.style.left = `${offsetX}px`;
     image.style.top = `${offsetY}px`;
+
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';
+
+    overlay.style.position = 'fixed';
+    overlay.style.left = `0px`;
+    overlay.style.top = `0px`;
+    overlay.style.width = '100vw';
+    overlay.style.height = '100vh';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+    overlay.style.zIndex = '1';
   }
 
   adjustImagePosition();
 
   window.addEventListener('resize', adjustImagePosition);
+
+  document.body.appendChild(overlay);
 });
