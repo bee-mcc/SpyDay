c4369bda350c4042dffa98eaf867e5d66c94917b
bee-mcc <tuj03791@temple.edu>
2024-05-11 13:52:58 -0400

    displays image

--- public/index.ts
+++ public/index.ts
@@ -1,49 +1,21 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('load', () => {
   const image = document.getElementById('smokey') as HTMLImageElement;
-  const canvas = document.querySelector('.canvas') as HTMLDivElement;
 
-  function adjustImageSize() {
-    const isDesktop = window.innerWidth > 768; // Assuming desktop width starts from 768px
-    const isLargerThanImage =
-      window.innerWidth > image.width ||
-      window.innerHeight > image.height;
+  function adjustImagePosition() {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const imageWidth = image.width;
+    const imageHeight = image.height;
 
-    if (isDesktop) {
-      if (isLargerThanImage) {
-        canvas.style.width = `${image.width}px`;
-        canvas.style.height = `${image.height}px`;
-        canvas.style.overflow = 'visible';
-        canvas.style.display = 'flex';
-        canvas.style.justifyContent = 'center';
-        canvas.style.alignItems = 'center';
-      } else {
-        const widthRatio = window.innerWidth / image.width;
-        const heightRatio = window.innerHeight / image.height;
-        const minRatio = Math.min(widthRatio, heightRatio);
+    const offsetX = (windowWidth - imageWidth) / 2;
+    const offsetY = (windowHeight - imageHeight) / 2;
 
-        canvas.style.width = `${window.innerWidth}px`;
-        canvas.style.height = `${window.innerHeight}px`;
-        canvas.style.overflow = 'hidden';
-        canvas.style.display = 'flex';
-        canvas.style.justifyContent = 'center';
-        canvas.style.alignItems = 'center';
-
-        const offsetX = (image.width - window.innerWidth) / 2;
-        const offsetY = (image.height - window.innerHeight) / 2;
-        image.style.left = `-${offsetX}px`;
-        image.style.top = `-${offsetY}px`;
-      }
-    } else {
-      canvas.style.width = `${image.width}px`;
-      canvas.style.height = `${image.height}px`;
-      canvas.style.overflow = 'visible';
-      canvas.style.display = 'flex';
-      canvas.style.justifyContent = 'center';
-      canvas.style.alignItems = 'center';
-    }
+    image.style.position = 'fixed';
+    image.style.left = `${offsetX}px`;
+    image.style.top = `${offsetY}px`;
   }
 
-  adjustImageSize();
+  adjustImagePosition();
 
-  window.addEventListener('resize', adjustImageSize);
+  window.addEventListener('resize', adjustImagePosition);
 });
